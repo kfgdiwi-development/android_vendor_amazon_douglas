c44d05fd76d13b97a872f7458903609b4ec3c3a6
daniel_hk <daniel.p6800@gmail.com>
2020-11-14 14:32:44 +0100

    douglas: Adapt for the camera wrapper.
    1. Add the dependences for building camera wrapper.
    2. Rename the modified libcam.halsensor to libcam2halsensor.

--- Android.mk
+++ Android.mk
@@ -22,4 +22,22 @@ LOCAL_MULTILIB = 32
 LOCAL_SRC_FILES_32 = proprietary/lib/libdpframework.so
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcam2halsensor
+LOCAL_SRC_FILES_64 := proprietary/lib64/libcam2halsensor.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libcam2halsensor.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcameracustom
+LOCAL_SRC_FILES_64 := proprietary/lib64/libcameracustom.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libcameracustom.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
 endif
